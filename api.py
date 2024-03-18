dcda4927c16aafc115210f3a13aa60394b0d66f6
Varol Cagdas Tok <95916060+ctokx@users.noreply.github.com>
2024-03-18 17:00:04 +0100

    Summary for large files is supported

--- api.py
+++ api.py
@@ -3,15 +3,60 @@ import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-
-
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.llms import OpenAI as LLamaopenai
+from llama_index.llms import ChatMessage, MessageRole
+from llama_index import ChatPromptTemplate
+from llama_index import StorageContext, load_index_from_storage 
+from llama_index.evaluation import FaithfulnessEvaluator
+from typing import Any
 load_dotenv()
-
+INPUT_DIR = 'data/input'
 api_key = os.getenv("OPENAI_API_KEY")
 if api_key is None:
     raise ValueError("OPENAI_API_KEY not found in environment variables.")
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
+
+def get_contextual_response(query: str) -> Any:
+    persist_dir = '/mytmp/index_storage/input_context'
+
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+    llm = LLamaopenai(model="gpt-3.5-turbo", temperature=0.1, api_key=OPENAI_API_KEY)
+    service_context = ServiceContext.from_defaults(llm=llm)
+
+
+    documents = SimpleDirectoryReader(INPUT_DIR).load_data()
+    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+
+
+    chat_text_qa_msgs = [
+        ChatMessage(
+            role=MessageRole.SYSTEM,
+            content="Summarize the text cover the main aspecs as deatiled as possible. give it in bullet points"
+        ),
+ 
+    ]
+
+    text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
+    query_engine = index.as_query_engine()
+    formatted_query = text_qa_template.format(query_str=query)
+
+    response = query_engine.query(formatted_query)
+
+    evaluator = FaithfulnessEvaluator(service_context=service_context)
+    eval_result = evaluator.evaluate_response(response=response)
+
+    if isinstance(response, str):
+        return response
+    elif hasattr(response, 'text'):
+        return response.text
+    elif isinstance(response, list):
+        return ' '.join([item.text if hasattr(item, 'text') else str(item) for item in response])
+    else:
+        return str(response)
 
 def summarize_with_gpt3(transcribed_text):
     response = client.chat.completions.create(
@@ -39,7 +84,15 @@ def transcribe_and_summarize(uploaded_file):
 
     transcribed_text = transcription.text
 
-    summary_text = summarize_with_gpt3(transcribed_text)
+    transcript_file_path = 'data/input/input.txt'
+
+    os.makedirs(os.path.dirname(transcript_file_path), exist_ok=True)
+
+
+    with open(transcript_file_path, 'w') as file:
+        file.write(transcribed_text)
+
+    summary_text = get_contextual_response("summarize the text")
 
     language_detected = "en"
     
